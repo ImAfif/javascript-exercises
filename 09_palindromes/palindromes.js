ec3a4773273a95b56fbab5216d05321f74b1e79b
Afif <71191421+ImAfif@users.noreply.github.com>
2022-04-24 11:37:58 -0400

    Update palindromes.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
+const palindromes = function (str) {
+
+    str = str.toLowerCase();
+    str = str.replace(",", "");
+    str = str.replace(".", "");
+    str = str.replace(":", "");
+    str = str.replace(";", "");
+    str = str.replace("-", "");
+    str = str.replace(",", "");
+    str = str.replace(" ", "");
+
+
+    for (let i = 0; i <= (str.length / 2); i++) {
+        if (str[i] != str[str.length - ( i + 1)]) return false 
+    }
+return true 
+
+    
 
 };
-
-// Do not edit below this line
-module.exports = palindromes;
